98350b6611f877a58755b2a816e86d6e5640e95c
sn0rii <113938479+sn0rii@users.noreply.github.com>
2023-05-10 07:37:14 +0200

    Add files via upload

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -3,12 +3,40 @@ import { data } from "../../../data";
 
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data);
+
+  const removeItems = (id) => {
+    // console.log(id);
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+  const clearAllItems = () => {
+    setPeople([]);
+  };
+
   return (
     <div>
       {people.map((person) => {
-        console.log(person);
-        return <h4>item</h4>;
+        const { id, name } = person;
+        {
+          /* console.log(person); */
+        }
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+            <button type="button" onClick={() => removeItems(id)}>
+              remove
+            </button>
+          </div>
+        );
       })}
+      <button
+        type="button"
+        style={{ marginTop: "2rem" }}
+        className="btn"
+        onClick={clearAllItems}
+      >
+        clear items
+      </button>
     </div>
   );
 };
