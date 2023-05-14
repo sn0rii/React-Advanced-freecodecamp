ef698996efa66779e99861603921494129ebe022
sn0rii <113938479+sn0rii@users.noreply.github.com>
2023-05-14 08:29:26 +0200

    Add files via upload

--- src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,34 @@
+import { useState } from "react";
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null);
+
+  const login = () => {
+    //normally connect to db or api
+    setUser({ name: "John" });
+  };
+  const logout = () => {
+    setUser(null);
+  };
+  return (
+    <div>
+      {user ? (
+        <div>
+          <h4>Hello there, {user.name}</h4>
+          <button className="btn" onClick={logout}>
+            logout
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h4>Please login</h4>
+          <button className="btn" onClick={login}>
+            login
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default UserChallenge;
