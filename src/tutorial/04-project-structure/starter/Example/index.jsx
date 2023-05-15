657005fcc030c353b0887f85d4cde5cfa1ced9bc
sn0rii <113938479+sn0rii@users.noreply.github.com>
2023-05-15 08:40:50 +0200

    Add files via upload

---
+++ src/tutorial/04-project-structure/starter/Example/index.jsx
@@ -0,0 +1,14 @@
+import FirstComponent from "./FirstComponent";
+import SecondComponent from "./SecondComponent";
+
+const Example = () => {
+  const handleChecker = () => {};
+  return (
+    <div>
+      <h2>Example</h2>
+      <FirstComponent />
+      <SecondComponent />
+    </div>
+  );
+};
+export default Example;
