863257f740fdcaa1f7b401925d9f8abf6d17463d
sn0rii <113938479+sn0rii@users.noreply.github.com>
2023-05-15 08:23:37 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import Starter from "./tutorial/01-useState/starter/03-useState-array";
+import { Home, About } from "./tutorial/04-project-structure/starter/Pages";
 
 function App() {
   return (
     <div className="container">
-      <Starter />
+      <Home />
+      <About />
     </div>
   );
 }
