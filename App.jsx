e6422f048d5b598d94d19466db9de7129b38f830
sn0rii <113938479+sn0rii@users.noreply.github.com>
2023-05-15 08:40:06 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import Example from "./tutorial/04-project-structure/starter/Example";
+import { Home, About } from "./tutorial/04-project-structure/starter/Pages";
+
+function App() {
+  return (
+    <div className="container">
+      <Example />
+    </div>
+  );
+}
+
+export default App;
