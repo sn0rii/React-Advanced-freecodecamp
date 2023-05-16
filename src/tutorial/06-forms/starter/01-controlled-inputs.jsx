32d9955b79cd7880bb4a851cc7c5e08ac0bd910c
sn0rii <113938479+sn0rii@users.noreply.github.com>
2023-05-16 07:45:38 +0200

    Add files via upload

---
+++ src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -0,0 +1,23 @@
+const ControlledInputs = () => {
+  return (
+    <form className="form">
+      <h4>controlled inputs</h4>
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input type="text" id="name" className="form-input" />
+      </div>
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          email
+        </label>
+        <input type="text" id="email" className="form-input" />
+      </div>
+      <button type="submit" className="btn btn-block">
+        submit
+      </button>
+    </form>
+  );
+};
+export default ControlledInputs;
