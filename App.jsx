2b74a41084432372eb92e068b019b52af8612bda
sn0rii <113938479+sn0rii@users.noreply.github.com>
2023-05-16 07:22:50 +0200

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,10 +1,10 @@
-import Example from "./tutorial/04-project-structure/starter/Example";
-import { Home, About } from "./tutorial/04-project-structure/starter/Pages";
+import List from "./tutorial/05-leverage-javascript/starter/List";
 
 function App() {
   return (
     <div className="container">
-      <Example />
+      <h2>Leverage Javascript</h2>
+      <List />
     </div>
   );
 }
