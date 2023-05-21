d8ac52b2c1bfa92ec30a3e308312491d64128455
sn0rii <113938479+sn0rii@users.noreply.github.com>
2023-05-21 08:24:57 +0200

    Add files via upload

--- src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,30 +1,57 @@
-import React from 'react';
-import { data } from '../../../data';
+import { useState, useReducer } from "react";
+import { data } from "../../../data";
+import { CLEAR_LIST, RESET_LIST, REMOVE_ITEM } from "./action";
+import reducer from "./reducer";
+
+const defaultState = {
+  people: data,
+  isLoading: false,
+};
+
 const ReducerBasics = () => {
-  const [people, setPeople] = React.useState(data);
+  const [state, dispatch] = useReducer(reducer, defaultState);
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
+    dispatch({ type: REMOVE_ITEM, payload: { id } });
   };
+
+  const clearList = () => {
+    dispatch({ type: CLEAR_LIST });
+  };
+
+  const resetList = () => {
+    dispatch({ type: RESET_LIST });
+  };
+  console.log(state);
+
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
-          <div key={id} className='item'>
+          <div key={id} className="item">
             <h4>{name}</h4>
             <button onClick={() => removeItem(id)}>remove</button>
           </div>
         );
       })}
-      <button
-        className='btn'
-        style={{ marginTop: '2rem' }}
-        onClick={() => setPeople([])}
-      >
-        clear items
-      </button>
+      {state.people.length < 1 ? (
+        <button
+          className="btn"
+          style={{ marginTop: "2rem" }}
+          onClick={resetList}
+        >
+          reset
+        </button>
+      ) : (
+        <button
+          className="btn"
+          style={{ marginTop: "2rem" }}
+          onClick={clearList}
+        >
+          clear items
+        </button>
+      )}
     </div>
   );
 };
